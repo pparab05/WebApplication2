cc0c6f7293d36c06878c8c4303e29a1eaa4da228
prathamesh <prathameshparab05@gmail.com>
2022-02-27 21:46:01 +0530

    commit

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApplication2
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World2");
             });
         }
     }
